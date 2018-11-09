c2e49f6a4334e26fee26cea403e1a0754a2dbb26
zpjune <1084013577@qq.com>
2018-11-09 17:53:24 +0800

    xiugai mima

--- STORE.Security/STORE.Security/SecurityHelper.cs
+++ STORE.Security/STORE.Security/SecurityHelper.cs
@@ -20,7 +20,24 @@ namespace STORE.Security
         public static Encoding DefaultEncoding { get; } = Encoding.UTF8;
 
         #endregion prop
+ public static string StringToMD5Hash(string inputString)
 
+        {
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+            byte[] encryptedBytes = md5.ComputeHash(Encoding.ASCII.GetBytes(inputString));
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < encryptedBytes.Length; i++)
+
+            {
+                sb.AppendFormat("{0:x2}", encryptedBytes[i]);
+            }
+            return sb.ToString();
+
+        }
        
         #region 对称加密算法
 
